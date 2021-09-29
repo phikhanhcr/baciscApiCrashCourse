cf3a598f1ff66f6233c4850694fa00dc3344aa8f
Khanh Phi <sykhanhsky@gmail.com>
2021-09-29 09:06:21 +0700

    first commit

---
+++ services/redis.js
@@ -0,0 +1,27 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', function() {
+    console.log('Redis Database connected'+'\n');
+});
+
+client.on('reconnecting', function() {
+    console.log('Redis client reconnecting');
+});
+
+client.on('ready', function() {
+    console.log('Redis client is ready');
+});
+
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+
+client.on('end', function() {
+    console.log('\nRedis client disconnected');
+    console.log('Server is going down now...');
+    process.exit();
+});
+
+
+exports.default = client
